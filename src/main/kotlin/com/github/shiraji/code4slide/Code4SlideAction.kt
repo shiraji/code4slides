0d8480bfd67724a7edaf5be05aa6eb4a115bf524
shiraji <isogai.shiraji@gmail.com>
2018-05-28 14:04:15 +0900

    Support opening multiple files in new window

--- src/main/kotlin/com/github/shiraji/code4slide/Code4SlideAction.kt
+++ src/main/kotlin/com/github/shiraji/code4slide/Code4SlideAction.kt
@@ -8,7 +8,11 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl
+import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorImpl
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.*
+import com.intellij.psi.util.*
+import org.intellij.plugins.relaxNG.compact.psi.util.PsiFunction
 import java.util.*
 
 class Code4SlideAction : AnAction() {
@@ -16,15 +20,31 @@ class Code4SlideAction : AnAction() {
         e ?: return
         val project = e.getRequiredData(CommonDataKeys.PROJECT);
         val manager = FileEditorManager.getInstance(project);
+        val editor = e.getData(CommonDataKeys.EDITOR) ?: return
+        val file = e.getData(CommonDataKeys.PSI_FILE) ?: return
+        val offset = editor.caretModel.offset
+        val element = file.findElementAt(offset) ?: return
 
-        val scratchFile = ScratchRootType.getInstance().createScratchFile(project, "scratchfile.kt", Language.findLanguageByID("kotlin"),
-                """
-                |if (flag) {
-                |    val foo = 1
-                |}""".trimMargin()) ?: return
+        val parent = PsiTreeUtil.getParentOfType(element, PsiIfStatement::class.java, PsiMethod::class.java, PsiClass::class.java) ?: return
+
+//        val parent = element.parentOfType(PsiIfStatement::class, PsiMethod::class, PsiClass::class) ?: return
+
+//        val selectedText = editor.selectionModel.selectedText ?: return
+        val scratchFile = ScratchRootType.getInstance().createScratchFile(project, "scratchfile.${file.language.associatedFileType?.defaultExtension}", file.language, parent.text) ?: return
 
         val openFileInNewWindow = (manager as? FileEditorManagerImpl)?.openFileInNewWindow(scratchFile) ?: return
 
-        openFileInNewWindow.first[0].component.getComponent(1)
+
+        openFileInNewWindow.first[0]
+//        container.add(EditorTabbedContainer.createDockableEditor(myProject, null, file, new Presentation(file.getName()), editorWindow), null);
+
+
+        val scratchFile2 = ScratchRootType.getInstance().createScratchFile(project, "scratchfile.kt", Language.findLanguageByID("kotlin"),
+                parent.text.replace("\n", "")) ?: return
+//
+//        manager.openFile(scratchFile2, false)
+        manager.openFileImpl2(manager.windows[1],scratchFile2, false)
+//                val fileEditor = openFileInNewWindow.second[0].createEditor(project, scratchFile2)
+//        openFileInNewWindow.first.set(1, fileEditor)
     }
 }
